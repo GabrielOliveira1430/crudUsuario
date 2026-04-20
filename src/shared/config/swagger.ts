962a79e516604c67f253e57ea3860c4322ba3bcf
Gabriel Oliveira <gabrielerika8724@gmail.com>
2026-04-20 19:22:47 -0300

    fix: swagger usando url correta em produção

--- src/shared/config/swagger.ts
+++ src/shared/config/swagger.ts
@@ -5,8 +5,19 @@ import { authSchemas } from '../../docs/schemas/auth.docs';
 import { authPaths } from '../../docs/paths/auth.path';
 
 export const swaggerSetup = (app: any) => {
+  const serverUrl =
+    process.env.NODE_ENV === 'production'
+      ? 'https://crudusuario-production.up.railway.app'
+      : 'http://localhost:3000';
+
   const document = {
     ...swaggerDocument,
+
+    servers: [
+      {
+        url: serverUrl + '/api/v1',
+      },
+    ],
 
     // ✅ Junta TODAS as rotas (users + auth)
     paths: {
