2ce453c8a8d51c70142857519870fb78b3fcbb71
Gabriel Oliveira <gabrielerika8724@gmail.com>
2026-04-22 08:29:53 -0300

    swagger corrigido

--- src/shared/config/swagger.ts
+++ src/shared/config/swagger.ts
@@ -5,13 +5,12 @@ export const swaggerSetup = (app: any) => {
   const isProduction = process.env.NODE_ENV === 'production';
 
   const serverUrl = isProduction
-    ? 'https://crudusuario-production.up.railway.app/api/v1'
+    ? 'https://api.coreauth.dev/api/v1'
     : 'http://localhost:3000/api/v1';
 
   const document = {
     ...swaggerDocument,
 
-    // 🔥 FIX DEFINITIVO
     servers: [
       {
         url: serverUrl,
@@ -21,7 +20,6 @@ export const swaggerSetup = (app: any) => {
 
   console.log('🔥 Swagger rodando em:', serverUrl);
 
-  // 🚀 GARANTE CACHE LIMPO DO SWAGGER UI
   app.use(
     '/docs',
     swaggerUi.serve,
